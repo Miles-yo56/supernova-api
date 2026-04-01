b477c9e916ad8c83d547f3adb27b445b312c2fc2
Miles-yo56 <gabrielsantosprodrigues85@gmail.com>
2026-04-01 20:27:32 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,23 @@
 
+# servidor.py
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Supernova API Online 🚀"
+
+@app.route("/perfil")
+def perfil():
+    # Exemplo simples de perfil
+    return jsonify({
+        "nome": "Gabriel",
+        "email": "gabrielsantosprodrigues85@gmail.com",
+        "foto": "https://i.pinimg.com/236x/23/Lz/4A/23Lz4AGye.jpg"
+    })
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
